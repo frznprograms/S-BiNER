780bbca19da8af116785dbede911138e672bde07
Shane Vivek <shane_vbharathan@aiip.sg>
2025-07-15 10:25:43 +0800

    feat: added more helper functions

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,4 +1,60 @@
+import torch
+import random
+from typing import Optional
+from loguru import logger
+
+
 def delist_the_list(items: list):
     for i in range(len(items)):
         items[i] = items[i][0]
     return items
+
+
+def set_device(device_type: str = "auto") -> str:
+    device = None
+    if device_type == "auto":
+        if torch.cuda.is_available():
+            device = "cuda"
+        elif torch.mps.is_available():
+            device = "mps"
+        else:
+            device = "cpu"
+
+    else:
+        if device_type == "cuda":
+            if torch.cuda.is_available():
+                device = "cuda"
+            else:
+                logger.warning(
+                    f"Could not set device to {device_type}, defaulting to cpu instead."
+                )
+                device = "cpu"
+        if device_type == "mps":
+            if torch.mps.is_available():
+                device = "mps"
+            else:
+                logger.warning(
+                    f"Could not set device to {device_type}, defaulting to cpu instead."
+                )
+                device = "cpu"
+        else:
+            device = "cpu"
+
+    if device is None:
+        raise ValueError(f"Could not assign device of type {device_type}")
+
+    return device
+
+
+def set_seeds(seed_num: Optional[int], deterministic: bool = True) -> None:
+    if seed_num is None:
+        seed_num = 42
+    torch.manual_seed(seed_num)
+    random.seed(seed_num)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed_num)
+        torch.cuda.manual_seed_all(seed_num)
+
+    if deterministic:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
