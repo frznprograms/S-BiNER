1c5ea65a065b7c3d75cfc62536110ec7596522ca
frznprograms <shanevbh@gmail.com>
2025-07-23 18:06:39 +0800

    refactor: changing the way the problem is framed - the fundamental problem with the current implementation:     - model does not actually consider word to word alignments; only considers whether a target word is aligned to some word in the source sentence     - model does not actually return any alignments in src_idx-tgt_idx form - need to change the model architecture to ensure that:     - the model is actually modelling the correct stuff     - the output is consistent with what we want: word alignments

--- src/models/binary_align_models.py
+++ src/models/binary_align_models.py
@@ -2,97 +2,180 @@ import torch
 import torch.nn as nn
 
 # import torch.nn.functional as F
-from typing import Optional
 from transformers import (
     RobertaModel,
     RobertaPreTrainedModel,
     XLMRobertaModel,
     XLMRobertaPreTrainedModel,
 )
-from src.models.binary_token_classification import (
-    BinaryTokenClassification,
-    SpanTokenAlignerOutput,
-)
+from src.models.binary_token_classification import BinaryTokenClassificationModel
 from src.configs.model_config import ModelConfig
 
 
 class RobertaModelForBinaryTokenClassification(
-    RobertaPreTrainedModel, BinaryTokenClassification
+    RobertaPreTrainedModel, BinaryTokenClassificationModel
 ):
     def __init__(self, config: ModelConfig):
-        roberta_config = config._to_roberta_config()
+        roberta_config = self.config._to_roberta_config()
         super().__init__(config=roberta_config)
-        self.num_labels = config.num_labels
-        # prevent pooling to get token-level outputs, not sentence-level
-        self.model = RobertaModel(config=roberta_config, add_pooling_layer=False)
-        classifier_dropout = (
-            config.classifier_dropout
-            if config.classifier_dropout is not None
-            else config.hidden_dropout_prob
-        )
-        self.dropout = nn.Dropout(classifier_dropout)
-        self.classifier = nn.Linear(in_features=config.hidden_size, out_features=1)
+        self.num_labels = self.config.num_labels
 
-        # initialise weights and final pre-processing
-        self.post_init()
+        # prevent pooling to get token-level, not sentence level outputs
+        self.encoder = RobertaModel(config=roberta_config, add_pooling_layer=False)
+
+        classifier_dropout = (
+            self.config.classifier_dropout
+            if self.config.classifier_dropout is not None
+            else self.config.hidden_dropout_prob
+        )
+
+        self.dropout = nn.Dropout(classifier_dropout)
+
+        # initialise weights and final processing
+        self.__post_init__()
 
     def forward(
         self,
-        input_ids: torch.Tensor,
-        attention_mask: torch.Tensor,
-        labels: Optional[torch.Tensor] = None,
-    ) -> SpanTokenAlignerOutput:
+        input_ids: torch.FloatTensor,
+        attention_mask: torch.FloatTensor,
+        source_mask: torch.BoolTensor,
+        target_mask: torch.BoolTensor,
+    ):
         return super(RobertaModelForBinaryTokenClassification, self).forward(
-            model=self.model,
             input_ids=input_ids,
             attention_mask=attention_mask,
-            labels=labels,
+            source_mask=source_mask,
+            target_mask=target_mask,
         )
 
     def get_input_embeddings(self):
-        return self.model.get_input_embeddings()
+        return self.encoder.get_input_embeddings()
 
     def set_input_embeddings(self, value):
-        self.model.set_input_embeddings(value)
+        self.encoder.set_input_embeddings(value)
+
+
+# class RobertaModelForBinaryTokenClassification(
+#     RobertaPreTrainedModel, BinaryTokenClassification
+# ):
+#     def __init__(self, config: ModelConfig):
+#         roberta_config = config._to_roberta_config()
+#         super().__init__(config=roberta_config)
+#         self.num_labels = config.num_labels
+#         # prevent pooling to get token-level outputs, not sentence-level
+#         self.model = RobertaModel(config=roberta_config, add_pooling_layer=False)
+#         classifier_dropout = (
+#             config.classifier_dropout
+#             if config.classifier_dropout is not None
+#             else config.hidden_dropout_prob
+#         )
+#         self.dropout = nn.Dropout(classifier_dropout)
+#         self.classifier = nn.Linear(in_features=config.hidden_size, out_features=1)
+
+#         # initialise weights and final pre-processing
+#         self.post_init()
 
 
 class XLMRobertaModelForBinaryTokenClassification(
-    XLMRobertaPreTrainedModel, BinaryTokenClassification
+    XLMRobertaPreTrainedModel, BinaryTokenClassificationModel
 ):
     def __init__(self, config: ModelConfig):
-        xlm_roberta_config = config._to_xlm_roberta_config()
-        super().__init__(config=xlm_roberta_config)
-        self.num_labels = config.num_labels
-        # prevent pooling to get token-level outputs, not sentence-level
-        self.model = XLMRobertaModel(config=xlm_roberta_config, add_pooling_layer=False)
-        classifier_dropout = (
-            config.classifier_dropout
-            if config.classifier_dropout is not None
-            else config.hidden_dropout_prob
-        )
-        self.dropout = nn.Dropout(classifier_dropout)
-        self.classifier = nn.Linear(in_features=config.hidden_size, out_features=1)
+        roberta_config = self.config._to_roberta_config()
+        super().__init__(config=roberta_config)
+        self.num_labels = self.config.num_labels
 
-        self.post_init()
+        # prevent pooling to get token-level, not sentence level outputs
+        self.encoder = XLMRobertaModel(config=roberta_config, add_pooling_layer=False)
+
+        classifier_dropout = (
+            self.config.classifier_dropout
+            if self.config.classifier_dropout is not None
+            else self.config.hidden_dropout_prob
+        )
+
+        self.dropout = nn.Dropout(classifier_dropout)
+
+        # initialise weights and final processing
+        self.__post_init__()
 
     def forward(
         self,
-        input_ids: torch.Tensor,
-        attention_mask: torch.Tensor,
-        labels: Optional[torch.Tensor] = None,
-    ) -> SpanTokenAlignerOutput:
+        input_ids: torch.FloatTensor,
+        attention_mask: torch.FloatTensor,
+        source_mask: torch.BoolTensor,
+        target_mask: torch.BoolTensor,
+    ):
         return super(XLMRobertaModelForBinaryTokenClassification, self).forward(
-            model=self.model,
             input_ids=input_ids,
             attention_mask=attention_mask,
-            labels=labels,
+            source_mask=source_mask,
+            target_mask=target_mask,
         )
 
     def get_input_embeddings(self):
-        return self.model.get_input_embeddings()
+        return self.encoder.get_input_embeddings()
 
     def set_input_embeddings(self, value):
-        self.model.set_input_embeddings(value)
+        self.encoder.set_input_embeddings(value)
+
+
+#     def forward(
+#         self,
+#         input_ids: torch.Tensor,
+#         attention_mask: torch.Tensor,
+#         labels: Optional[torch.Tensor] = None,
+#     ) -> SpanTokenAlignerOutput:
+#         return super(RobertaModelForBinaryTokenClassification, self).forward(
+#             model=self.model,
+#             input_ids=input_ids,
+#             attention_mask=attention_mask,
+#             labels=labels,
+#         )
+
+#     def get_input_embeddings(self):
+#         return self.model.get_input_embeddings()
+
+#     def set_input_embeddings(self, value):
+#         self.model.set_input_embeddings(value)
+
+
+# class XLMRobertaModelForBinaryTokenClassification(
+#     XLMRobertaPreTrainedModel, BinaryTokenClassification
+# ):
+#     def __init__(self, config: ModelConfig):
+#         xlm_roberta_config = config._to_xlm_roberta_config()
+#         super().__init__(config=xlm_roberta_config)
+#         self.num_labels = config.num_labels
+#         # prevent pooling to get token-level outputs, not sentence-level
+#         self.model = XLMRobertaModel(config=xlm_roberta_config, add_pooling_layer=False)
+#         classifier_dropout = (
+#             config.classifier_dropout
+#             if config.classifier_dropout is not None
+#             else config.hidden_dropout_prob
+#         )
+#         self.dropout = nn.Dropout(classifier_dropout)
+#         self.classifier = nn.Linear(in_features=config.hidden_size, out_features=1)
+
+#         self.post_init()
+
+#     def forward(
+#         self,
+#         input_ids: torch.Tensor,
+#         attention_mask: torch.Tensor,
+#         labels: Optional[torch.Tensor] = None,
+#     ) -> SpanTokenAlignerOutput:
+#         return super(XLMRobertaModelForBinaryTokenClassification, self).forward(
+#             model=self.model,
+#             input_ids=input_ids,
+#             attention_mask=attention_mask,
+#             labels=labels,
+#         )
+
+#     def get_input_embeddings(self):
+#         return self.model.get_input_embeddings()
+
+#     def set_input_embeddings(self, value):
+#         self.model.set_input_embeddings(value)
 
 
 # TODO: find out if it is better to use RobertaModelForTokenClassification
--- src/models/binary_token_classification.py
+++ src/models/binary_token_classification.py
@@ -1,41 +1,103 @@
 from dataclasses import dataclass
-from typing import Optional, Union
+from typing import Union
 import torch
 import torch.nn as nn
 from transformers import RobertaPreTrainedModel, XLMRobertaPreTrainedModel
+from src.configs.model_config import ModelConfig
 
 
 @dataclass
-class SpanTokenAlignerOutput:
-    logits: Optional[torch.Tensor] = None
-    loss: Optional[torch.Tensor] = None
+class BinaryTokenClassificationModel(nn.Module):
+    encoder: Union[RobertaPreTrainedModel, XLMRobertaPreTrainedModel]
+    config: ModelConfig
 
-
-class BinaryTokenClassification(nn.Module):
-    classifier: nn.Linear
-    dropout: nn.Dropout
+    def __post_init__(self):
+        super().__init__()
+        # for [src || tgt] concatenation
+        self.classifier = nn.Linear(self.config.hidden_size * 2, out_features=1)
 
     def forward(
         self,
-        model: Union[RobertaPreTrainedModel, XLMRobertaPreTrainedModel],
-        input_ids: torch.Tensor,
-        attention_mask: torch.Tensor,
-        labels: Optional[torch.Tensor] = None,
-    ) -> SpanTokenAlignerOutput:
-        last_hidden_state: torch.Tensor = model(
-            input_ids, attention_mask=attention_mask
-        )[0]  # shape is (batch_size, seq_len, hidden_layer_dim=768)
-        logits = self.classifier(self.dropout(last_hidden_state)).to(torch.float32)
+        input_ids: torch.FloatTensor,
+        attention_mask: torch.FloatTensor,
+        source_mask: torch.BoolTensor,
+        target_mask: torch.BoolTensor,
+    ):
+        """
+        Arguments:
+            input_ids: Tensor of shape (B, seq_len)
+            attention_mask: Tensor of shape (B, seq_len)
+            src_mask: Bool Tensor of shape (B, seq_len) with 1s at source positions
+            tgt_mask: Bool Tensor of shape (B, seq_len) with 1s at target positions
 
-        loss = None
-        if labels is not None:  # i.e. only for training, not inference
-            new_labels = torch.where(labels == -100, 0.0, labels)
-            # return loss for each individual token; no averaging yet:
-            loss_func = nn.BCEWithLogitsLoss(reduction="none")
-            loss = loss_func(logits.view(-1), new_labels.view(-1))
-            # zero out loss for ignored tokens:
-            loss = torch.where(labels.view(-1) == -100, 0, loss)
-            # average loss over valid tokens:
-            loss = torch.sum(loss) / (labels != -100).sum()
+        Returns:
+            logits: Tensor of shape (B, src_len, tgt_len) with logits for each src-tgt token pair
+        """
+        # get the contextual token representations
+        sequence_output = self.encoder(
+            input_ids=input_ids, attention_mask=attention_mask
+        ).last_hidden_state  # (B, seq_len, H)
+        # get source and target token embeddings using boolean masks
+        source_repr = self.apply_mask(
+            sequence_output, source_mask
+        )  # (B, source_len, H)
+        target_repr = self.apply_mask(
+            sequence_output, target_mask
+        )  # (B, target_len, H)
 
-        return SpanTokenAlignerOutput(loss=loss, logits=logits)
+        B, S, H = source_repr.shape
+        T = target_repr.shape[1]
+
+        # create source-target pairwise combinations
+        source_exp = source_repr.unsqueeze(2).expand(B, S, T, H)
+        target_exp = target_repr.unsqueeze(2).expand(B, S, T, H)
+        combined = torch.cat([source_exp, target_exp], dim=-1)  # (B, S, T, 2H)
+        logits = self.classifier(combined).squeeze(-1)  # (B, S, T)
+
+        return logits
+
+    def apply_mask(self, sequence_output, mask: torch.BoolTensor):
+        B, L, H = sequence_output.size()
+        token_lists = []
+        for i in range(B):
+            indices = mask[i].nonzero(as_tuple=True)[0]
+            token_lists.append(sequence_output[i, indices, :])
+
+        return nn.utils.rnn.pad_sequence(token_lists, batch_first=True)
+
+
+# @dataclass
+# class SpanTokenAlignerOutput:
+#     logits: Optional[torch.Tensor] = None
+#     loss: Optional[torch.Tensor] = None
+
+
+# class BinaryTokenClassification(nn.Module):
+#     classifier: nn.Linear
+#     dropout: nn.Dropout
+
+#     def forward(
+#         self,
+#         model: Union[RobertaPreTrainedModel, XLMRobertaPreTrainedModel],
+#         input_ids: torch.Tensor,
+#         attention_mask: torch.Tensor,
+#         labels: Optional[torch.Tensor] = None,
+#     ) -> SpanTokenAlignerOutput:
+#         last_hidden_state: torch.Tensor = model(
+#             input_ids, attention_mask=attention_mask
+#         )[0]  # shape is (batch_size, seq_len, hidden_layer_dim=768)
+#         logits = self.classifier(self.dropout(last_hidden_state)).to(torch.float32)
+
+#         loss = None
+#         if labels is not None:  # i.e. only for training, not inference
+#             new_labels = torch.where(labels == -100, 0.0, labels)
+#             # return loss for each individual token; no averaging yet:
+#             loss_func = nn.BCEWithLogitsLoss(reduction="none")
+#             loss = loss_func(logits.view(-1), new_labels.view(-1))
+#             # zero out loss for ignored tokens:
+#             loss = torch.where(labels.view(-1) == -100, 0, loss)
+
+#             # average loss over valid tokens:
+#             loss = torch.sum(loss) / (labels != -100).sum()
+
+#         return SpanTokenAlignerOutput(loss=loss, logits=logits)
