f55242c7da06c5239e37bc6bd84b58be217c0dd8
frznprograms <shanevbh@gmail.com>
2025-09-03 15:12:26 +0800

    refactor: included more features as part of SBinerNERDataset init
    - Dataset will now include all the resources necessary for efficient
    batching
    - no longer need to have complex logic to prepare all the elements
    required for annotaion projection; just init the dataset, pass to
    NERHeuristics and let internal methods prepare data further for NER

--- src/datasets/ner_dataset.py
+++ src/datasets/ner_dataset.py
@@ -2,12 +2,31 @@ from torch.utils.data import Dataset
 
 
 class SBinerNERDataset(Dataset):
-    def __init__(self, source_sentences, target_sentences):
+    def __init__(
+        self,
+        source_sentences: list[str],
+        target_sentences: list[str],
+        source_labels: list[list[str]],
+        alignments: list[list[tuple[int, int]]],
+    ):
+        assert (
+            len(self.source_sentences)
+            == len(self.target_sentences)
+            == len(source_labels)
+            == len(alignments)
+        ), "Please ensure the list are the same length."
         self.source_sentences = source_sentences
         self.target_sentences = target_sentences
+        self.source_labels = source_labels
+        self.alignments = alignments
 
     def __len__(self):
         return len(self.target_sentences)
 
-    def __getitem__(self, idx: int):
-        return self.target_sentences[idx]
+    def __getitem__(self, i: int) -> tuple[str, str, list[str], list[tuple[int, int]]]:
+        return (
+            self.source_sentences[i],
+            self.target_sentences[i],
+            self.source_labels[i],
+            self.alignments[i],
+        )
